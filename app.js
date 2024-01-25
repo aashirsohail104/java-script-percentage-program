ee0770d288fa072853fd16e43af16354cd3c40b5
AASHIR SIDDIQUI <154249806+aashirsohail104@users.noreply.github.com>
2024-01-25 15:11:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+ //If Statement
+
+const ptr = prompt("Tell me Your Percentage")
+
+if (ptr<= 50) {
+    console.log("Your are Pass. Phr bhi prh le bhai")
+} else if(ptr<= 60){
+    console.log("Grade B")
+}else if(ptr<= 70){
+    console.log("Grade A")
+}else if(ptr<= 80){
+    console.log("Grade A-1")
+}else if(ptr<= 100){
+    console.log("Grade A-1+")
+}else{
+    console.log("Please Input the correct data")
+}
+
